bab325e3860e10cee47a95496fbf3d9cf945561c
Chad Pearson <G00254194@stu.raritanval.edu>
2017-11-20 12:49:11 -0500

    8.49
    Added a main method to start the game

---
+++ Game_Start.java
@@ -0,0 +1,23 @@
+
+/**
+ * Write a description of class MainGameStart here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Game_Start
+{
+    /**
+     * The method below creates the game object and runs the play method inside of it
+     */
+    
+    
+    public Game_Start()
+    {
+        // initialise instance variables
+        
+        Game game = new Game();
+        game.play();
+    
+    }
+}
